6a1dbb44406ce06c2d0f17b55efe5884e45c0617
Suraj Thapa <surajthapart@gmail.com>
2024-08-22 20:18:59 +0545

    file structured setup done

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className= "flex-1 bg-gray-600">
-      <Text>this screen.</Text>
+    <View className= "flex-1 bg-zinc-100 flex items-center justify-center">
+      <Text className="font-pblack">this screen.</Text>
     </View>
   );
 }
